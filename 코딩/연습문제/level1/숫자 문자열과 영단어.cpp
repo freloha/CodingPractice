efda857bfabca96966f41dc88554ed838ebfdff2
Samkim <hskim9406@naver.com>
2022-03-24 16:56:40 +0900

    Update 숫자 문자열과 영단어.cpp

--- 코딩/연습문제/level1/숫자 문자열과 영단어.cpp
+++ 코딩/연습문제/level1/숫자 문자열과 영단어.cpp
@@ -70,15 +70,3 @@ int solution(string s) {
     int ret = stoi(answer);
     return ret;
 }
-
-/*
-*#include <bits/stdc++.h>
-*using namespace std;
-*int solution(string s) {
-*    vector<string> patterns = { "zero","one","two","three","four","five","six","seven","eight","nine" };
-*    string num = "0123456789";
-*    for (int i = 0; i < num.size(); i++) 
-*        s = regex_replace(s, regex(patterns[i]), num.substr(i,1));
-*    return stoi(s);
-*}
-*/
