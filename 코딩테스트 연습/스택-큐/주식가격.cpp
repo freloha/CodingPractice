e174dd8f37c622f7ffe9d7096ffab9083b4af016
Samkim <hskim9406@naver.com>
2022-03-26 00:29:55 +0900

    Create 주식가격.cpp

---
+++ 코딩테스트 연습/스택-큐/주식가격.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+    vector<int> answer;
+    int first = 0;
+    
+    for(int i = 0; i < prices.size(); i++){
+        int count = 0;
+        for(int j = i + 1; j < prices.size(); j++){
+            count++;
+            if(prices[i] > prices[j]){
+                break;
+            }
+        }
+        answer.push_back(count);
+    }
+    
+    return answer;
+}
