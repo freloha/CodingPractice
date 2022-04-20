712b4ef7bfd152455cb1d9f9a9f4e21c380d2678
Samkim <hskim9406@naver.com>
2022-04-20 18:33:22 +0900

    2022.04.20

---
+++ 코딩 테스트를 위한 자료 구조와 알고리즘 with C++/(017)priority_queue를 이용한 median구하기.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+struct median
+{
+	priority_queue<int> maxHeap; // -> priority_queue<int, vector<int>, less<int>> maxHeap; 과 같은 의미
+	priority_queue<int, vector<int>, greater<int>> minHeap; // 이 사용법이 약간 생소, greater는 내림차순, 9,8,7,6,5,4,3,2,1
+
+	void insert(int data) {
+		if (maxHeap.size() == 0) {
+			maxHeap.push(data);
+			return;
+		}
+
+		if (maxHeap.size() == minHeap.size()) {
+			if (data <= get())
+				maxHeap.push(data);
+			else
+				minHeap.push(data);
+
+			return;
+		}
+
+		if (maxHeap.size() < minHeap.size()) {
+			if (data > get()) {
+				maxHeap.push(minHeap.top());
+				minHeap.pop();
+				minHeap.push(data);
+			}
+			else
+				maxHeap.push(data);
+			return;
+		}
+
+		if (data < get()) {
+			minHeap.push(maxHeap.top());
+			maxHeap.pop();
+			maxHeap.push(data);
+		}
+		else
+			minHeap.push(data);
+	}
+
+	double get() {
+		if (maxHeap.size() == minHeap.size())
+			return (maxHeap.top() + minHeap.top()) / 2.0;
+		if (maxHeap.size() < minHeap.size())
+			return minHeap.top();
+		return maxHeap.top();
+	}
+};
+
+int main() {
+	median med;
+
+	med.insert(1);
+	cout << "1 삽입 후 중앙값 : " << med.get() << endl;
+
+	med.insert(5);
+	cout << "5 삽입 후 중앙값 : " << med.get() << endl;
+
+	med.insert(2);
+	cout << "2 삽입 후 중앙값 : " << med.get() << endl;
+
+	med.insert(10);
+	cout << "10 삽입 후 중앙값 : " << med.get() << endl;
+
+	med.insert(40);
+	cout << "40 삽입 후 중앙값 : " << med.get() << endl;
+}
